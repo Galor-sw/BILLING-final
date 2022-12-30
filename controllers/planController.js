5f264b8c194dd85b78ef70f3a376d805fc8ce85f
Alona <alonarozner4@gmail.com>
2022-12-30 21:21:04 +0200

    update the functions "get all plans" and "get plan by name" from mongo arrange the UI

--- controllers/planController.js
+++ controllers/planController.js
@@ -1,65 +1,61 @@
 const { Plan } = require('../models/plan')
 
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-const stripe = require('stripe')(stripeSecretKey);
+// const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+// const stripe = require('stripe')(stripeSecretKey);
 
 module.exports = {
-    getAllPlans: async (req, res) => {
-        let jsonProducts = {};
-        try {
-            const products = await stripe.products.list();
-            jsonProducts.data = [];
-            for (let i in products.data) {
-                let jsonProduct = {};
-                if (products.data[i].active) {
-
-                    jsonProduct.id = products.data[i].id;
-                    jsonProduct.name = products.data[i].name;
-                    jsonProducts.data[i] = await getPricesForProduct(jsonProduct);
-                }
-            }
-            res.send(jsonProducts);
-        } catch (e) {
-            console.log("error " + e.message);
-        }
+    getAllPlans: (req, res) => {
+        Plan.find({}).then(result => {
+            res.send(result);
+        })
+            .catch(err => loggers.error(err));
     },
-
-    purchasePlan: async (req, res) => {
-        try {
-            const session = await stripe.checkout.sessions.create({
-                success_url: 'http://localhost:5000/message',
-                cancel_url: 'http://localhost:5000/message',
-                line_items: [
-                    {price: req.body.id, quantity: req.body.quantity},
-                ],
-                mode: 'subscription',
-            })
-            const urlCheckOut = session.url;
-            res.send(urlCheckOut);
-        } catch (e) {
-            console.log("error " + e.message);
-        }
+    getPlanByName: async (req,res) => {
+        Plan.findOne({'name':name}).then(result=>{
+            if (result) {
+                res.send(result);
+            } else {
+                res.send("Does not exist, try again");
+            }
+        }).catch(err => loggers.error(err));
     }
 }
-
-const getPricesForProduct = async (product) => {
-    product.prices = [];
-    try {
-        const price = await stripe.prices.search({
-            query: `product:\'${product.id}\'`,
-        });
-        for (let i in price.data) {
-            product.prices[i] = {};
-            product.prices[i].id = price.data[i].id;
-            product.prices[i].price = price.data[i].unit_amount / 100;
-            if (price.data[i].type == 'recurring')
-                product.prices[i].interval = price.data[i].recurring.interval;
-            if (price.data[i].type == 'one_time')
-                product.prices[i].interval = price.data[i].type;
-        }
-        return product;
-
-    } catch (e) {
-        console.log("error " + e.message);
-    }
-}
+    // purchasePlan: async (req, res) => {
+    //     try {
+    //         const session = await stripe.checkout.sessions.create({
+    //             success_url: 'http://localhost:5000/message',
+    //             cancel_url: 'http://localhost:5000/message',
+    //             line_items: [
+    //                 {price: req.body.id, quantity: req.body.quantity},
+    //             ],
+    //             mode: 'subscription',
+    //         })
+    //         const urlCheckOut = session.url;
+    //         res.send(urlCheckOut);
+    //     } catch (e) {
+    //         console.log("error " + e.message);
+    //     }
+    // }
+// }
+//
+// const getPricesForProduct = async (product) => {
+//     product.prices = [];
+//     try {
+//         const price = await stripe.prices.search({
+//             query: `product:\'${product.id}\'`,
+//         });
+//         for (let i in price.data) {
+//             product.prices[i] = {};
+//             product.prices[i].id = price.data[i].id;
+//             product.prices[i].price = price.data[i].unit_amount / 100;
+//             if (price.data[i].type == 'recurring')
+//                 product.prices[i].interval = price.data[i].recurring.interval;
+//             if (price.data[i].type == 'one_time')
+//                 product.prices[i].interval = price.data[i].type;
+//         }
+//         return product;
+//
+//     } catch (e) {
+//         console.log("error " + e.message);
+//     }
+// }
--- routers/planRouter.js
+++ routers/planRouter.js
@@ -2,8 +2,10 @@ const { Router } = require('express');
 const planController = require('../controllers/planController');
 const planRouter = Router();
 
-planRouter.get('/', planController.getAllPlans)
-planRouter.post('/', planController.purchasePlan)
+planRouter.get('/', planController.getAllPlans);
+planRouter.get('/', planController.getPlanByName);
+
+// planRouter.post('/', planController.purchasePlan);
 
 
 module.exports = planRouter;
