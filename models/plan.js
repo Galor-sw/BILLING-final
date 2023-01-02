dead880c0e98387c095f33bce7e3f4c895074ab2
DorHaimovich1 <dorh@usearch.com>
2023-01-02 20:58:19 +0200

    changed the schema

--- models/plan.js
+++ models/plan.js
@@ -1,14 +1,25 @@
 const {Schema, model} = require("mongoose");
 
+const priceSchema = new Schema({
+    stripeID: {type: String, require: true, unique: true},
+    amount: {type: Number, require: true},
+    interval: {type: String, require: true, unique: true},
+    intervalCount: {type: Number, require: true},
+
+}, {collection: 'plans', versionKey: false})
+
 const planSchema = new Schema({
-    name: {type: String, require: true, unique: true},
-    price_month: {type: Number, require: true},
-    price_year: {type: Number, require: false},
+    stripeID: {type: String, require: true, unique: true},
+    name: {type: String, require: true},
     seats: {type: Number, require: true},
-    features: {type: [String], require: true},
-    credits: {type: Number, require: true}
+    features: [{type: String, require: true}],
+    credits: {type: Number, require: true},
+    description: {type: String},
+    prices: {priceSchema},
+
+
 }, {collection: 'plans', versionKey: false})
 
 const Plan = model('plans', planSchema);
 
-module.exports = { Plan };
+module.exports = {Plan};
