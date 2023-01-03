866ada08839bdd59c1aa4a359d46034f1d5a5a71
itay45977 <itay45977@gmail.com>
2023-01-03 17:04:17 +0200

    Fixed.

--- routers/planRouter.js
+++ routers/planRouter.js
@@ -4,6 +4,7 @@ const planRouter = Router();
 
 planRouter.get('/', planController.getAllPlans);
 planRouter.post('/', planController.purchasePlan);
+planRouter.get('/:name', planController.getPlanByName);
 
 
 module.exports = planRouter;
