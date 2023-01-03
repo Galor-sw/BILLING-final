b37549991d20a3f561ed2a24e5b958be39baf0f1
Alona <alonarozner4@gmail.com>
2023-01-03 20:09:13 +0200

    statistic for this dat, month and year.

---
+++ routers/statisticRouter.js
@@ -0,0 +1,9 @@
+const {Router} = require('express');
+const statisticController = require('../controllers/statisticController');
+
+const statisticRouter = Router();
+statisticRouter.get('/day/', statisticController.getMRRforDay);
+statisticRouter.get('/month', statisticController.getMRRforMonth);
+statisticRouter.get('/year/', statisticController.getMRRforYear);
+
+module.exports = statisticRouter;
