e17dd664b3aa99fbf4cc5db9ae74967451be4894
galor <galor546@gmail.com>
2023-01-05 14:01:15 +0200

    queue2 added (send suspended account id)
    cron job set to the new function names

--- RMQ/rabbitMQ.js
+++ RMQ/rabbitMQ.js
@@ -1,15 +1,17 @@
 const amqp = require('amqplib/callback_api');
-const amqpURL = process.env.AMQP_URL;
+const amqpSendPlanURL = process.env.AMQP_SEND_PLAN_TO_IAM_URL;
+const amqpSuspendedAccountURL = process.env.AMQP_SEND_SUSPENDED_ACCOUNT_TO_IAM_URL;
 const serverLogger = require(`../logger`);
 const logger = serverLogger.log;
 
-const sendQueueUpdate = (user_mail, credits) => {
-
-    amqp.connect(amqpURL, (err, conn) => {
+const sendPlanDetailsToIAM = (account_id, credits, seats, feauters) => {
+    amqp.connect(amqpSendPlanURL, (err, conn) => {
         conn.createChannel(async (err, ch) => {
             const q = 'CloudAMQP';
             const subscriptionDetails = {
-                "user_mail": {user_mail},
+                "account_id": {account_id},
+                "seats": {seats},
+                "features": {feauters},
                 "credits": {credits}
             };
             const stringMsg = JSON.stringify(subscriptionDetails);
@@ -21,5 +23,21 @@ const sendQueueUpdate = (user_mail, credits) => {
     })
 }
 
-module.exports = sendQueueUpdate;
+const sendSuspendedAccountToIAM = (account_id) => {
+    amqp.connect(amqpSuspendedAccountURL, (err, conn) => {
+        conn.createChannel(async (err, ch) => {
+            const q = 'CloudAMQP';
+            const subscriptionDetails = {
+                "account_id": {account_id},
+                "flag": 'suspended'
+            };
+            const stringMsg = JSON.stringify(subscriptionDetails);
+            ch.assertQueue(q, {durable: false});
+            await ch.sendToQueue(q, Buffer.from(stringMsg));
+            logger.info(`RMQ- suspended account message sent to user: ${account_id}`);
+
+        })
+    })
+}
+module.exports = sendPlanDetailsToIAM, sendSuspendedAccountToIAM;
 
