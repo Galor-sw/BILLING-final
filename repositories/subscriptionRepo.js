e459994aabb0ca4d04de0eb3d8ba357a108b77c5
itay45977 <itay45977@gmail.com>
2023-01-07 03:31:39 +0200

    Added edit subscription by mongo _id to update the sub

--- repositories/subscriptionRepo.js
+++ repositories/subscriptionRepo.js
@@ -1,24 +1,31 @@
-const {Subscription} = require('../models/subscriptions')
+const { Subscription } = require('../models/subscriptions');
 
 module.exports = {
-    createSubscription: async (newSub) => {
-        const newSubscription = new Subscription(newSub);
-        return await newSubscription.save();
-    },
+  createSubscription: async (newSub) => {
+    const newSubscription = new Subscription(newSub);
+    return await newSubscription.save();
+  },
 
-    getAllPSubscription: async () => {
-        return Subscription.find({}).populate({path: "plan", model: "plans"});
-    },
+  getAllSubscription: async () => {
+    return Subscription.find({}).populate({ path: 'plan', model: 'plans' });
+  },
 
-    getSubscriptionByID: async (ID) => {
-        return Subscription.findById(ID).populate({path: "plan", model: "plans"});
-    },
+  getSubscriptionByID: async (ID) => {
+    return Subscription.findById(ID).populate({ path: 'plan', model: 'plans' });
+  },
 
-    getAllSubscriptionsByPlanID: async (plan) => {
-        return Subscription.find({plan: plan}).populate({path: "plan", model: "plans"});
-    },
+  getAllSubscriptionsByPlanID: async (plan) => {
+    return Subscription.find({ plan }).populate({ path: 'plan', model: 'plans' });
+  },
 
-    getSubscriptionByClientID: async (ID) => {
-        return Subscription.findOne({accountId: ID}).populate({path: "plan", model: "plans"});
-    },
-}
+  getSubscriptionByClientID: async (ID) => {
+    return Subscription.findOne({ accountId: ID }).populate({ path: 'plan', model: 'plans' });
+  },
+  editSubscription: async (id, sub) => {
+    return Subscription.findByIdAndUpdate(id, sub, { new: true })
+      .populate({
+        path: 'plan',
+        model: 'plans'
+      });
+  }
+};
