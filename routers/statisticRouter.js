4f6eb81e33bf29128815b184f5c73420c3a8587a
galor <galor546@gmail.com>
2023-01-07 22:43:19 +0200

    statistics set routes correct MRR - motnh ARR - year DRR - day

--- routers/statisticRouter.js
+++ routers/statisticRouter.js
@@ -2,8 +2,8 @@ const { Router } = require('express');
 const statisticController = require('../controllers/statisticController');
 
 const statisticRouter = Router();
-statisticRouter.get('/DRR', statisticController.getMRRforDay);
-statisticRouter.get('/MRR', statisticController.getMRR);
-statisticRouter.get('/ARR', statisticController.getARR);
+statisticRouter.get('/ARR/:year', statisticController.getARR);
+statisticRouter.get('/MRR/:year/:month', statisticController.getMRR);
+statisticRouter.get('/DRR/:year/:month/:day', statisticController.getDRR);
 
 module.exports = statisticRouter;
