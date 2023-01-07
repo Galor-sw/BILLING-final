d0a178e8dfecebaeb029252335fe40178f6a25eb
itay45977 <itay45977@gmail.com>
2023-01-07 03:37:59 +0200

    Created cases for each event. Checkout session has the accountId through metadata. Saves the details from invoice event - and stores it in the DB. Sends mail to the user by nodemailer I've created. Have to add some functionality to the other events.

--- controllers/webhooksController.js
+++ controllers/webhooksController.js
@@ -1,80 +1,93 @@
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const stripe = require('stripe')(stripeSecretKey);
-const nodemailer = require("nodemailer");
-module.exports = {
-    getEvents: async (req, res) => {
+const nodemailer = require('nodemailer');
+const plansRepo = require('../repositories/plansRepo');
+const subsRepo = require('../repositories/subscriptionRepo');
+const serverLogger = require('../logger');
+const logger = serverLogger.log;
 
-        const payload = req.body;
-        const sig = req.headers['stripe-signature'];
-        const endpointSecret = process.env.STRIPE_ENDPOINT_SECRETKEY;
-
-        let event;
-
-        try {
-            event = stripe.webhooks.constructEvent(payload, sig.toString(), endpointSecret)
-        } catch (err) {
-            console.log(err.message)
-            res.status(400).json({success: false})
-            return;
-        }
-
-        console.log(event.type);
-        console.log(event.data.object);
-        console.log(event.data.object.id);
-    },
-    start: async (req, res) => {
-        res.send("Welcome");
-    },
+module.exports = (() => {
+  const invoiceMap = {};
+  return ({
     getEvent: async (req, res) => {
-        const endpointSecret = "whsec_XYwueoi7ozW6fVlvmZUiKHiD4EMuuI18";
-        const sig = req.headers['stripe-signature'];
+      const endpointSecret = process.env."STRIPE_SECRET_KEY ";
+      const sig = req.headers['stripe-signature'];
+      let event;
+      let session = '';
+      try {
+        event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+      } catch (err) {
+        res.status(404).send(`Webhook Error: ${err.message}`);
+        return;
+      }
 
-        let event;
-        let session = "";
-        try {
-            event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-        } catch (err) {
-            res.status(400).send(`Webhook Error: ${err.message}`);
-            return;
-        }
+      switch (event.type) {
+        case 'checkout.session.completed':
+          if (!invoiceMap[event.data.object.invoice]) {
+            invoiceMap[event.data.object.invoice] = event.data.object.metadata.account;
+          }
+          break;
+        case 'charge.expired':
+          session = event.data.object;
+          break;
+        case 'charge.succeeded':
+          session = event.data.object;
+          break;
+        case 'invoice.payment_succeeded':
+          session = event.data.object;
 
-        // Handle the event
-        switch (event.type) {
-            case 'charge.expired':
-                session = event.data.object;
-                // Then define and call a function to handle the event charge.expired
-                break;
-            case 'charge.succeeded':
-                session = event.data.object;
-                // Then define and call a function to handle the event charge.succeeded
-                break;
-            case "checkout.session.async_payment_succeeded":
-                session = event.data.object;
+          const id = invoiceMap[session.id];
+          delete invoiceMap[session.id];
+          const end = new Date(session.lines.data[0].period.end);
+          const start = new Date(session.lines.data[0].period.start);
+          try {
+            const subscription = await subsRepo.getSubscriptionByClientID(id);
+            const plan = await plansRepo.getPlanByStripeId(session.lines.data[0].plan.product);
+            console.log(plan._id.toString());
+            const jsonObj = {
+              accountId: id,
+              plan: plan._id.toString(),
+              start_date: start,
+              next_date: end,
+              payment: session.lines.data[0].plan.interval,
+              renewal: end,
+              status: session.status
+            };
+            console.log(await subsRepo.editSubscription(subscription._id.toString(), jsonObj));
+            logger.info(`${id} subscription was updated.`);
+          } catch (err) {
+            logger.error(`failed to Update the repository: ${err.message}`);
+            res.status(404).send(err.message);
+          }
 
-                // let emailTo = session.customer_details.email;
-                let transporter = nodemailer.createTransport({
-                    host: "zohomail.com",
-                    service: "Zoho",
-                    secureConnection: false,
-                    auth: {
-                        user: process.env.email,
-                        pass: process.env.password
-                    },
-                });
-                let info = await transporter.sendMail({
-                    from: process.env.email,
-                    to: "itay45977@gmail.com",
-                    subject: "Thank you for your purchase",
-                    text: "Hey, thank you for your purchase",
-                    // html: `Hello ${session.customer_details.email} thanks for your purchase`,
-                    html: 'Hello'
-                });
-            // ... handle other event types
-            default:
-                console.log(`Unhandled event type ${event.type}`);
-        }
+          const transporter = nodemailer.createTransport({
+            host: 'zohomail.com',
+            service: 'Zoho',
+            secureConnection: false,
+            auth: {
+              user: process.env.email,
+              pass: process.env.password
+            }
+          });
 
-        // Return a 200 response to acknowledge receipt of the event
-        res.status(200).send();
+          try {
+            await transporter.sendMail({
+              from: process.env.email,
+              to: session.customer_email,
+              subject: 'Thank you for your purchase',
+              text: 'Hey, thank you for your purchase',
+              html: `Hello ${session.customer_name}, thanks for your purchase`
+            });
+          } catch (err) {
+            logger.error(`failed to send payment mail to client: ${err.message}`);
+          }
+
+          break;
+        case 'invoice.payment_failed':
+          session = event.data.object;
+          break;
+      }
+      res.status(200).send();
     }
-}
+  });
+})();
