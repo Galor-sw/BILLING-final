23a5d558da7d942f8477540dadc263619c5df4c0
itay45977 <itay45977@gmail.com>
2023-01-07 17:15:16 +0200

    Created a schedule - have not finished yet. added functionality to invoice failed

--- controllers/webhooksController.js
+++ controllers/webhooksController.js
@@ -26,6 +26,33 @@ module.exports = (() => {
           if (!invoiceMap[event.data.object.invoice]) {
             invoiceMap[event.data.object.invoice] = event.data.object.metadata.account;
           }
+          const checkoutSession = req.body.data;
+          const schedule = await stripe.subscriptionSchedules.create({
+            from_subscription: checkoutSession.subscription
+          });
+          console.log('Schedule created : ' + schedule.id);
+          const phases = schedule.phases.map(phase => ({
+            start_date: phase.start_date,
+            end_date: phase.end_date,
+            items: phase.items
+          }));
+          schedule = await stripe.subscriptionSchedules.update(
+            schedule.id,
+            {
+              end_behavior: 'cancel',
+              phases: [
+                ...phases,
+                {
+                  items: [{
+                    price: '', //needs to be price_id of stripe
+                    quantity: 1
+                  }],
+                  iterations: 12 // need to check whats the interval - month/year
+                }
+              ]
+            }
+            console.log(schedule);
+          );
           break;
         case 'charge.expired':
           session = event.data.object;
@@ -51,7 +78,7 @@ module.exports = (() => {
               next_date: end,
               payment: session.lines.data[0].plan.interval,
               renewal: end,
-              status: session.status
+              status: 'active'
             };
             console.log(await subsRepo.editSubscription(subscription._id.toString(), jsonObj));
             logger.info(`${id} subscription was updated.`);
@@ -85,6 +112,29 @@ module.exports = (() => {
           break;
         case 'invoice.payment_failed':
           session = event.data.object;
+          const sessionId = invoiceMap[session.id];
+          delete invoiceMap[session.id];
+          const endDate = new Date(session.lines.data[0].period.end);
+          const startDate = new Date(session.lines.data[0].period.start);
+          try {
+            const subscription = await subsRepo.getSubscriptionByClientID(id);
+            const plan = await plansRepo.getPlanByStripeId(session.lines.data[0].plan.product);
+            const jsonObj = {
+              accountId: sessionId,
+              plan: plan._id.toString(),
+              start_date: startDate,
+              next_date: endDate,
+              payment: session.lines.data[0].plan.interval,
+              renewal: endDate,
+              status: 'failed'
+            };
+            console.log(await subsRepo.editSubscription(subscription._id.toString(), jsonObj));
+            logger.info(`${id} subscription was suspended.`);
+          } catch (err) {
+            logger.error(`failed to Update the repository: ${err.message}`);
+            res.status(404).send(err.message);
+          }
+
           break;
       }
       res.status(200).send();
