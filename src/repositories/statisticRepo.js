373aafc09fc6efc9bb4a618af33b85e632d57fd1
DorHaimovich1 <dorh@usearch.com>
2023-02-19 17:25:39 +0200

    minor changes

--- src/repositories/statisticRepo.js
+++ src/repositories/statisticRepo.js
@@ -7,13 +7,15 @@ module.exports = {
       created: { gte: startRangeTimestamp, lte: endRangeTimestamp },
       limit: 100 // Maximum limit (10 is default)
     });
+    
     // paymentIntents returned by Stripe API are in cents, so we divide by 100
     const paymentIntents = paymentIntentsInCents.data
       .filter((paymentIntent) => paymentIntent.status === 'succeeded')
       .map((paymentIntent) => {
         return paymentIntent.amount / 100;
       });
+
     const amountTotal = paymentIntents.reduce((a, b) => a + b);
-    return await amountTotal;
+    return amountTotal;
   }
 };
--- src/routers/statisticRouter.js
+++ src/routers/statisticRouter.js
@@ -2,8 +2,8 @@ const { Router } = require('express');
 const statisticController = require('../controllers/statisticController');
 
 const statisticRouter = Router();
-statisticRouter.get('/ARR/:year', statisticController.getARR);
-statisticRouter.get('/MRR/:year/:month', statisticController.getMRR);
-statisticRouter.get('/DRR/:year/:month/:day', statisticController.getDRR);
+statisticRouter.get('/arr/:year', statisticController.getARR);
+statisticRouter.get('/mrr/:year/:month', statisticController.getMRR);
+statisticRouter.get('/drr/:year/:month/:day', statisticController.getDRR);
 
 module.exports = statisticRouter;
