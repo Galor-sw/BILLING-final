f62ee8184039a46d610f173f7c10ec0ba394146d
GilDavidi <95719086+GilDavidi@users.noreply.github.com>
2023-02-25 21:47:26 +0200

    update feature dynamic from api growth add constant file add GROWTH_API_URL to env add pattern to scheduledJob edit startCronJob function

--- src/repositories/plansRepo.js
+++ src/repositories/plansRepo.js
@@ -1,15 +1,21 @@
-const { Plan } = require('../models/plan');
+const {Plan} = require('../models/plan');
 module.exports = {
 
-  getPlans: () => {
-    return Plan.find({});
-  },
-  
-  getPlanByName: (planName) => {
-    return Plan.findOne({ name: planName });
-  },
+    getPlans: () => {
+        return Plan.find({});
+    },
 
-  getPlanByStripeId: (id) => {
-    return Plan.findOne({ StripeID: id });
-  }
+    getPlanByName: (planName) => {
+        return Plan.findOne({name: planName});
+    },
+
+    getPlanByStripeId: (id) => {
+        return Plan.findOne({StripeID: id});
+    },
+    updateFeaturesByPlanName: async (planName, features) => {
+        await Plan.updateOne(
+            {name: planName},
+            {$set: {features: features}}
+        )
+    }
 };
