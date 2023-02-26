47ced07d792a85ff82d4ff9e9e6f282a8bd4f223
DorHaimovich1 <dorh@usearch.com>
2023-02-26 02:48:27 +0200

    removed old purchase a plan function and route

--- src/routers/planRouter.js
+++ src/routers/planRouter.js
@@ -3,6 +3,5 @@ const planController = require('../controllers/planController');
 const planRouter = Router();
 
 planRouter.get('/', planController.getAllPlans);
-planRouter.post('/:accountId', planController.purchasePlan);
 
 module.exports = planRouter;
