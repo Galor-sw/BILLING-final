f61ea0f10f36566551686c05e5ec406203308946
itay45977 <itay45977@gmail.com>
2023-02-27 04:19:42 +0200

    Removed old files from phase1

--- public/js/message.js
+++
@@ -1,5 +0,0 @@
-const changeLocation = () => {
-  document.location.href = 'https://iam-shenkar.onrender.com';
-};
-
-setTimeout(changeLocation, 5000);
--- public/js/urlService.js
+++
@@ -1,3 +0,0 @@
-export const url = () => {
-  return 'http://localhost:5000';
-};
