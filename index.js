6fc56a21d07aee95de718b5afb553a8789341df6
DorHaimovich1 <dorh@usearch.com>
2023-02-27 03:58:07 +0200

    minor changes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config({ path: '.env' });
+
 const Server = require('./src/server');
 const startCronJob = require('./src/cronJob/cronJob');
 const listenQueue = require('./src/RMQ/reciverQueueMessage');
 const connectDb = require('./src/mongoConnection');
 const constant = require('./src/constants/constants');
-const { loggers } = require('winston');
+const Logger = require('abtest-logger');
+
+const logger = new Logger(process.env.CORE_QUEUE);
+
 const initServer = async () => {
   try {
     await connectDb();
@@ -12,7 +16,7 @@ const initServer = async () => {
     await startCronJob.scheduledJob(constant.CRON_JOB_FREQ);
     await new Server().start();
   } catch (err) {
-    loggers.error(`error in init server ${err.message}`);
+    logger.error(`error in init server ${err.message}`);
   }
 };
 
